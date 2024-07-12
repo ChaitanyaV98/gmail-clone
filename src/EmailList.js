457457fd2c937f9e56d98496b61004738a756f0e
Chaitanya <chaitanya@Chaitanyas-MacBook-Air.local>
2024-07-12 12:34:50 +0530

    fetch the data from firebase and show the real data in emaillsit

--- src/EmailList.js
+++ src/EmailList.js
@@ -11,12 +11,29 @@ import {
   Settings,
 } from "@mui/icons-material";
 import { IconButton, Checkbox } from "@mui/material";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./EmailList.css";
 import Section from "./Section";
 import EmailRow from "./EmailRow";
+import { db } from "./firebase";
+import { collection, query, onSnapshot, orderBy } from "firebase/firestore";
+import { Timestamp } from "firebase/firestore";
 
 function EmailList() {
+  const [emails, setEmails] = useState([]);
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      query(collection(db, "emails"), orderBy("timestamp", "desc")),
+      (snapshot) => {
+        setEmails(
+          snapshot.docs.map((doc) => ({ id: doc.id, data: doc.data() }))
+        );
+      }
+    );
+    return () => unsubscribe();
+  }, []);
+
   return (
     <div className="emailList">
       <div className="emailList__settings">
@@ -52,19 +69,30 @@ function EmailList() {
         <Section Icon={People} title="Social" color="#1a73e8" />
         <Section Icon={LocalOffer} title="Promotions" color="green" />
       </div>
+      {/* <div className="emailList__list">
+        {emails.map(({ id, data: { to, subject, message, timestamp } }) => (
+          <EmailRow
+            key={id}
+            id={id}
+            title={to}
+            subject={subject}
+            description={message}
+            time={timestamp}
+          />
+        ))}
+      </div> */}
       <div className="emailList__list">
-        <EmailRow
-          title="Twich"
-          subject="Hellow fellow streamer"
-          description="This is a test"
-          time="10pm"
-        />
-        <EmailRow
-          title="Twich"
-          subject="Hellow fellow streamer"
-          description="This is a testThis is a testThis is a test This is a test This is a test This is a test"
-          time="10pm"
-        />
+        {emails.map(({ id, data: { to, subject, message, timestamp } }) => (
+          <EmailRow
+            key={id}
+            id={id}
+            title={to}
+            subject={subject}
+            description={message}
+            // time={new Date(timestamp?.seconds * 1000).toLocaleTimeString()} // Formatting timestamp
+            time={new Date(timestamp?.seconds * 1000).toUTCString()}
+          />
+        ))}
       </div>
     </div>
   );
