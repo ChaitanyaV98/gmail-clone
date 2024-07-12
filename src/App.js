2f576ae9763b4f5fc2775b4c7a2cf094e77135ef
Chaitanya <chaitanya@Chaitanyas-MacBook-Air.local>
2024-07-12 14:29:57 +0530

    added login persist functionality i.e if the user is already loged then on refresh the need not login again

--- src/App.js
+++ src/App.js
@@ -1,20 +1,43 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import EmailList from "./EmailList";
 import Mail from "./Mail";
+import { login } from "./features/userSlice.js";
 import SendMail from "./SendMail";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { selectSendMessageIsOpen } from "./features/mailSlice";
 import { selectUser } from "./features/userSlice";
 import Login from "./Login.js";
+import { auth } from "./firebase";
+import { onAuthStateChanged } from "firebase/auth";
 
 function App() {
   const sendMessageIsOpen = useSelector(selectSendMessageIsOpen);
 
   const user = useSelector(selectUser);
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    // onAuthStateChanged listens to authentication state changes
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is logged in
+        dispatch(
+          login({
+            displayName: user.displayName,
+            email: user.email,
+            photoUrl: user.photoURL,
+          })
+        );
+      }
+    });
+
+    // Cleanup subscription on unmount
+    return () => unsubscribe();
+  }, [dispatch]);
   return (
     <Router>
       {!user ? (
